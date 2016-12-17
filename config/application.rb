28f1cb7f58fdfe29b806e71cf7659e35fb37cf0e
b-i-hide <b.i1993patriots@gmail.com>
2016-12-18 00:09:55 +0900

    settings to avoid unnecessary files

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec,
+          fixture: true,
+          fixture_replacement: :factory_girl,
+          routing_specs: false,
+          helpers_spec: false,
+          integration_tool: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
