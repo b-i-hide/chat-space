a5a1a33c513c6e533b5f9599d9e9875a35af01bb
b-i-hide <b.i1993patriots@gmail.com>
2017-01-08 00:20:56 +0900

    test of user mdoel

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'with valid attributes' do
+      it "is valid with all attributes" do
+        expect(build(:user)).to be_valid
+      end
+    end
+
+    context 'with invalid attributes' do
+      it "is invalid without name" do
+        user = build(:user, name: nil)
+        user.valid?
+        expect(user.errors[:name]).to include("を入力してください")
+      end
+
+      it "is invalid without email" do
+        user = build(:user, email: nil)
+        user.valid?
+        expect(user.errors[:email]).to include("を入力してください")
+      end
+
+      it "is invalid to use same email" do
+        user = create(:user, email: 'hoge@hoge.com')
+        new_user = build(:user, email: 'hoge@hoge.com')
+        new_user.valid?
+        expect(new_user.errors[:email]).to include("はすでに存在します")
+      end
+
+      it "is invalid without password" do
+        user = build(:user, password: nil)
+        user.valid?
+        expect(user.errors[:password]).to include("を入力してください")
+      end
+
+      it "is invalid to use password less than 7 characters" do
+        user = build(:user, password: 0000000)
+        user.valid?
+        expect(user.errors[:password]).to include("は6文字以上で入力してください")
+      end
+
+      it "is same as password" do
+        user = build(:user, password: 00000000 ,password_confirmation: 11111111)
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
+      end
+    end
+  end
 end
