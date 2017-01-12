dcf7f8e92fa2fa95d9879273ce4f3a2b5153c820
b-i-hide <b.i1993patriots@gmail.com>
2017-01-12 23:50:29 +0900

    run migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110090656) do
+ActiveRecord::Schema.define(version: 20170111072033) do
+
+  create_table "chat_group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "chat_group_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["chat_group_id"], name: "index_chat_group_users_on_chat_group_id", using: :btree
+    t.index ["user_id"], name: "index_chat_group_users_on_user_id", using: :btree
+  end
 
   create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 20170110090656) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "chat_group_users", "chat_groups"
+  add_foreign_key "chat_group_users", "users"
 end
