4d6929225c3d070e33e0eb001c545d65a8592d3e
b-i-hide <b.i1993patriots@gmail.com>
2017-01-14 02:12:59 +0900

    edit message model and controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
+  def index
+    @message = Message.new
+    @messages = Message.where(chat_group_id: params[:chat_group_id])
+    @groups = ChatGroup.all
+    @group = ChatGroup.find(params[:chat_group_id])
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @groups = ChatGroup.all
+    if @message.save
+      redirect_to chat_group_messages_path(@group), notice: 'メッセージが投稿されました'
+    else
+      redirect_to chat_group_messages_path(@group), alert: 'メッセージが投稿されませんでした'
+    end
+  end
+
+  private
+
+  def set_group
+    @group = ChatGroup.find(params[:chat_group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :image, :user_id, :chat_group_id)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_group
+
+  (validates :body, presence: true) || (validates :image, presence: true)
+
+  def posted_date_format
+    self.created_at.strftime("%Y/%m/%d %X")
+  end
 end
