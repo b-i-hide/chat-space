d78de1fa76362f9d0507cb166361eaba8fff4c03
b-i-hide <b.i1993patriots@gmail.com>
2017-01-14 02:37:15 +0900

    set method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,15 @@
 class MessagesController < ApplicationController
   before_action :set_group, only: [:index, :create]
+  before_action :set_all_groups, only:  [:index, :create]
+
   def index
     @message = Message.new
     @messages = Message.where(chat_group_id: params[:chat_group_id])
-    @groups = ChatGroup.all
     @group = ChatGroup.find(params[:chat_group_id])
   end
 
   def create
     @message = Message.new(message_params)
-    @groups = ChatGroup.all
     if @message.save
       redirect_to chat_group_messages_path(@group), notice: 'メッセージが投稿されました'
     else
@@ -18,9 +18,12 @@ class MessagesController < ApplicationController
   end
 
   private
-
   def set_group
     @group = ChatGroup.find(params[:chat_group_id])
+  end
+
+  def set_all_groups
+    @groups = ChatGroup.all
   end
 
   def message_params
