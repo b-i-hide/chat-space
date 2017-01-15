6715ac10732c04ccd40b6932a99c0d27ab2079b3
b-i-hide <b.i1993patriots@gmail.com>
2017-01-15 18:15:38 +0900

    add validation

--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -2,4 +2,7 @@ class ChatGroup < ApplicationRecord
   has_many :chat_group_users
   has_many :users, through: :chat_group_users
   has_many :messages
+
+  validates :name, presence: true
+  validates :user_ids, presence: true
 end
