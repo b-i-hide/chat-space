c1ad0dd6d80f3227ddff994c439d5c6a31c4c954
b-i-hide <b.i1993patriots@gmail.com>
2017-01-16 16:50:28 +0900

    create validator and set path

--- app/validators/body_validator.rb
+++ app/validators/body_validator.rb
@@ -1,9 +1,15 @@
-class MessageValidator < ActiveModel::EachValidator
-  def validate_each
-    if record.body.present?
-      validates_presence_of(:body)
-    else
-      validates_presence_of(:image)
-    end
+class BodyValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors[attribute]<< (options[:message] || '投稿出来ませんでした') unless record.body? || record.image?
+    # record.errors.add(attribute, 'メッセージが投稿されませんでした') unless record.body? || record.image?
   end
+  # TODO コメントアウトは後で削除する
+  # def validate(record)
+  #   if record.body.blank?
+  #     record.errors[:body]<< (options[:message] || 'メッセージを投稿出来ませんでした')
+  #     # record.errors[:image]<< (options[:message] || '写真を投稿出来ませんでした')
+  #   elsif record.image.blank?
+  #     record.errors[:image]<< (options[:message] || '写真を投稿出来ませんでした')
+  #   end
+  # end
 end
