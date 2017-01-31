1141e2f13d0c23787d5a8b947975cc3bb6530672
b-i-hide <b.i1993patriots@gmail.com>
2017-01-31 22:50:23 +0900

    use this for formobject

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,7 +19,8 @@ $(function () {
 
   $('.js-form').on('submit',function (e) {
     e.preventDefault();
-    var fd = new FormData($(".js-form")[0]);
+    var jsForm = this;
+    var fd = new FormData(jsForm);
     var ajaxUrl = $('.js-form').attr('action');
     $.ajax({
       type: 'POST',
@@ -27,11 +28,11 @@ $(function () {
       data: fd,
       processData: false, // json形式でデータを渡すためにfalseに設定する
       contentType: false,　// ajax関数を使っているので、content-typeヘッダが自動で設定されない様にする
-      dataType: 'json',
+      dataType: 'json'
     })
     .done(function (data) {
       buildHTML(data);
-      $('.js-form')[0].reset();
+      jsForm.reset();
       $('.submit-button').attr('disabled', false);
     })
     .fail(function() {
